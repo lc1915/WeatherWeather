305d522c5b6c829e90e1ac53f4ee430b1716590d
lc1915 <liuchang1915@gmail.com>
2015-05-23 23:25:32 +0800

    2015.05.23

--- src/main/java/unique/weatherweather/MainActivity.java
+++ src/main/java/unique/weatherweather/MainActivity.java
@@ -2,19 +2,52 @@ package unique.weatherweather;
 
 import android.app.ActionBar;
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 
 
 public class MainActivity extends Activity {
 
+    private Button clickButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.navigation_layout);
 
+        setImmersiveMode();
+
+        clickButton = (Button) findViewById(R.id.click_btn);
+        clickButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setClass(MainActivity.this, TryAcitivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        setImmersiveMode();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    //set immersive mode
+    public void setImmersiveMode() {
         View decorView = getWindow().getDecorView();
         //Hide the status bar
         int uiOptions = View.SYSTEM_UI_FLAG_LAYOUT_STABLE
@@ -26,15 +59,9 @@ public class MainActivity extends Activity {
         decorView.setSystemUiVisibility(uiOptions);
         // Remember that you should never show the action bar if the
         // status bar is hidden, so hide that too if necessary.
+        // aaa
         ActionBar actionBar = getActionBar();
         actionBar.hide();
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
     }
 
     @Override
---
+++ src/main/java/unique/weatherweather/TryAcitivity.java
@@ -0,0 +1,17 @@
+package unique.weatherweather;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+
+/**
+ * Created by liuchang on 15-5-23.
+ */
+public class TryAcitivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
