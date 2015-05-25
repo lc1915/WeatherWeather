37c0d0104440aa55bf2a3a317e0073192e356dc5
lc1915 <liuchang1915@gmail.com>
2015-05-25 23:30:00 +0800

    2015.05.25

--- src/main/java/unique/weatherweather/MainActivity.java
+++ src/main/java/unique/weatherweather/MainActivity.java
@@ -17,7 +17,7 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.navigation_layout);
+        setContentView(R.layout.navigation_layout); //search the codes of navigationDrawer layout
 
         setImmersiveMode();
 
